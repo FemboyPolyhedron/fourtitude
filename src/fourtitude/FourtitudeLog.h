b3e3524c58394491ffb2477870731a1142a1874a
QQtheElephant <elephantcountry.qq@gmail.com>
2026-03-30 11:33:48 +0900

    fix: removed ai artifacts from vibecoding

--- src/fourtitude/FourtitudeLog.h
+++ src/fourtitude/FourtitudeLog.h
@@ -13,23 +13,18 @@ typedef enum
 } FOUR_LogLevel;
 
 typedef struct FOUR_Logger FOUR_Logger;
-
-/* Global setup */
-void          FOUR_LogSetFile(const char* path);
-
-/* Request a named logger (creates one if it doesn't exist yet) */
+ 
+void FOUR_LogSetFile(const char* path);
+ 
 FOUR_Logger*  FOUR_GetLogger(const char* name);
-
-/* Core log function */
+ 
 void FOUR_Logger_Log(FOUR_Logger* logger, FOUR_LogLevel level, const char* fmt, ...);
-
-/* Convenience macros */
+ 
 #define FOUR_DEBUG(logger, ...) FOUR_Logger_Log((logger), FOUR_LOG_DEBUG, __VA_ARGS__)
 #define FOUR_INFO(logger, ...)  FOUR_Logger_Log((logger), FOUR_LOG_INFO,  __VA_ARGS__)
 #define FOUR_WARN(logger, ...)  FOUR_Logger_Log((logger), FOUR_LOG_WARN,  __VA_ARGS__)
 #define FOUR_ERROR(logger, ...) FOUR_Logger_Log((logger), FOUR_LOG_ERROR, __VA_ARGS__)
-
-/* Legacy: logs at INFO level under the "Fourtitude" logger */
+ 
 void FOUR_Log(const char* fmt, ...);
 
 #ifdef __cplusplus
